4521e7a8191873740607c085e19f10185a2d114a
serhat.tarhan <tarhan.werek@gmail.com>
2024-01-24 11:19:06 +0300

    202401241118 Init Project

---
+++ src/main/java/com/tarhan/LoggerService/LoggerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tarhan.LoggerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerServiceApplication.class, args);
+	}
+
+}
